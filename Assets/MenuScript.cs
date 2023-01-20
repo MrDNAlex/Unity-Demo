f382b23b62b01628dfb17eacbe62d1c079d133ba
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2023-01-20 14:12:29 -0500

    Commit

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -11,22 +11,8 @@ namespace Program
     {
         [SerializeField] Button settings;
         [SerializeField] Button demo;
-        [SerializeField] Transform trans;
-        [SerializeField] Rigidbody rb;
-        [SerializeField] Collider coll;
-        [SerializeField] Image img;
-        [SerializeField] VerticalLayoutGroup vert;
-        Vector3 vect3 = new Vector3(1, 2, 3);
-
-        Vector2 vect2 = new Vector2(1, 2);
-
-        int age = 10;
-        float angle = 65.7f;
-        string name = "Alex";
-
-
-
-
+        //[SerializeField] Transform trans;
+       
         // Start is called before the first frame update
         void Start()
         {
@@ -36,28 +22,11 @@ namespace Program
             demo.onClick.AddListener(goToDemo);
 
 
-            float val = 0;
-            val = vect3.x;
-            val = vect3.y;
-            val = vect3.z;
-            val = vect3.magnitude;
-
-
-            vect3.Set(1, 2, 3);
-
-           
-          
-    
     }
 
         // Update is called once per frame
         void Update()
         {
-
-            GameObject.Find("Settings").GetComponent<Button>();
-
-
-
 
 
         }
@@ -73,67 +42,6 @@ namespace Program
             //Changes the scene to the Demo scene.
             SceneManager.LoadScene(2);
         }
-
-
-        void doNothing ()
-        {
-
-        }
-
-        void notNothing (string name, int age)
-        {
-            name = "";
-            age = 20;
-
-
-
-        }
-
-        int getAge ()
-        {
-
-            int age = 20;
-
-            return age;
-
-        }
-
-       void something ()
-        {
-
-            return;
-
-            //The code below return; will never run.
-
-        }
-
-
-
-
-
-        animal lion = new animal("Lion", 20);
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-    public class animal
-    {
-
-        public animal (string name, int age)
-        {
-
-        }
-
-
 
     }
 
---
+++ Assets/NewMenuScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using FlexUI;
+
+
+public class NewMenuScript : MonoBehaviour
+{
+    [SerializeField] Button settings;
+    [SerializeField] Button demo;
+
+    //FlexUI addition
+    [SerializeField] RectTransform backgroundTrans;
+    [SerializeField] RectTransform titleTrans;
+    [SerializeField] RectTransform startTrans;
+    [SerializeField] RectTransform settingsTrans;
+
+
+    //[SerializeField] Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Adds a listener to the Settings button
+        settings.onClick.AddListener(goToSettings);
+        //Adds a listener to the Start button.
+        demo.onClick.AddListener(goToDemo);
+
+        menuSetup();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void goToSettings()
+    {
+        //Changes the scene to the settings scene. 
+        SceneManager.LoadScene(1);
+    }
+
+    public void goToDemo()
+    {
+        //Changes the scene to the Demo scene.
+        SceneManager.LoadScene(2);
+    }
+
+    public void menuSetup ()
+    {
+        Flex backgroundObj = new Flex(backgroundTrans, 1);
+        Flex titleObj = new Flex(titleTrans, 1.5f);
+        Flex startObj = new Flex(startTrans, 1);
+        Flex settingsObj = new Flex(settingsTrans, 1);
+
+        backgroundObj.addChild(titleObj);
+        backgroundObj.addChild(startObj);
+        backgroundObj.addChild(settingsObj);
+
+        backgroundObj.setVerticalPadding(3, 1, 3, 1);
+
+        backgroundObj.setHorizontalPadding(1, 1, 1, 1);
+
+        backgroundObj.setSpacingFlex(2, 1);
+
+        startObj.setSelfHorizontalPadding(0.2f, 1, 0.2f, 1);
+        settingsObj.setSelfHorizontalPadding(0.2f, 1, 0.2f, 1);
+
+        backgroundObj.setSize(new Vector2(Screen.width, Screen.height));
+
+
+    }
+
+
+
+}
